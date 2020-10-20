86dbe645253d247140ce7e8a2d10a971cddfd4ef
Akshay9923 <akshaygelani786@gmail.com>
2020-10-20 11:57:17 +0530

    Improved Video-Chat Functionality

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
-const express = require('express');
-const app = express();
-const server = require('http').Server(app);
-const io = require('socket.io')(server);
+const express = require('express')
+const app = express()
+const server = require('http').Server(app)
+const io = require('socket.io')(server)
 const { ExpressPeerServer } = require('peer');
 const peerServer = ExpressPeerServer(server, {
     debug: true
@@ -17,15 +17,23 @@ app.get('/', (req, res) => {
     res.redirect(`/${uuidV4()}`)
 })
 
-app.get('/:roomIdParam', (req, res) => {
-    res.render('room', { roomId: req.params.roomIdParam })
+app.get('/:room', (req, res) => {
+    res.render('room', { roomId: req.params.room })
 })
 
-io.on("connection", socket => {
-
+io.on('connection', socket => {
     socket.on('join-room', (roomId, userId) => {
-        socket.join(roomId);
+        socket.join(roomId)
         socket.to(roomId).broadcast.emit('user-connected', userId);
+        // messages
+        socket.on('message', (message) => {
+            //send message to the same room
+            io.to(roomId).emit('createMessage', message)
+        });
+
+        socket.on('disconnect', () => {
+            socket.to(roomId).broadcast.emit('user-disconnected', userId)
+        })
     })
 })
 
